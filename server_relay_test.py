d1467cbaaf0b415796cb53ea46079019fda97a92
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2023-03-24 11:08:56 +0900

    test용 api파일

---
+++ server_relay_test.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/send', methods=['POST'])
+def send():
+    # Get JSON data from request
+    data = request.get_json()
+
+    # Send JSON data to external server
+    response = requests.post('localhost:5001/receive', json=data)
+
+    # Return response from external server to client
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
