3799e85700e92d2c28c8bb5491890e9be30269a2
kjunwoo23 <kjunwoo23@naver.com>
2023-04-07 19:34:06 +0900

    캐릭터 이동 + 작은 맵
    첫 커밋

---
+++ Assets/Scripts/Toy.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toy : MonoBehaviour
+{
+    public float freeMoveSpeed;
+    float reDirCnt;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ToyRandMove(60));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+
+    }
+
+    public IEnumerator ToyRandMove(float t)
+    {
+        reDirCnt = 0;
+        Vector3 dir = new Vector3(0, 0, 0);
+        while (t > 0)
+        {
+            if (reDirCnt < 0)
+            {
+                dir = new Vector3(Random.Range(-1f, 1f) * Random.Range(0f, freeMoveSpeed) * Time.fixedDeltaTime, 0, Random.Range(-1f, 1f) * Random.Range(0f, freeMoveSpeed) * Time.fixedDeltaTime);
+                transform.rotation = Quaternion.Euler(0, Mathf.Rad2Deg * Mathf.Atan(dir.x/dir.z) + 90, 0);
+                reDirCnt = Random.Range(0, 3f);
+            }
+            transform.position += dir;
+            yield return new WaitForFixedUpdate();
+            t -= Time.fixedDeltaTime;
+            reDirCnt -= Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            reDirCnt = 0;
+        }
+    }
+}
