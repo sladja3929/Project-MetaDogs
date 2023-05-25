5efd4799286faee575e7a3f7b30cf86af0d428e6
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2023-05-25 16:32:13 +0900

    ai model 수정에 따른 api 변경 v2

--- ml_api/src/mlagent.py
+++ ml_api/src/mlagent.py
@@ -15,33 +15,39 @@ MLAgent = Namespace(
 
 env_name = "/mlagent/Project-MetaDogs/Linux-build/AITest.x86_64"
 model_name = "/mlagent/Project-MetaDogs/AItest.yaml"
+result_path = "/mlagent/usr_model"
 
 @MLAgent.route('/create_model')
 class Create_Model(Resource):
     def post(self):
         # Get pet_token
         data = request.get_json()
-        model_int = f"model_{data['gesture_id']}"
 
-        run_id = data['pet_token'] + "_model_" + data['gesture_id']
+        run_id = f"{result_path}/{data['pet_token']}/model_{data['gesture_id']}"
 
+        #첫 학습
         cmd = f"mlagents-learn {model_name} --run-id={run_id} --env={env_name} --no-graphics"
         os.system(cmd)
 
 @MLAgent.route('/learning')
 class Learning(Resource):
     def post(self):
-        # Get data from request
-        model = request.files['model']
-        pet_token = request.form['pet_token']
-        gesture_id = request.form['gesture_id']
+        # Get pet_token
+        data = request.get_json()
 
-        run_id = pet_token + "_model_" + gesture_id
+        old_run_id = f"{result_path}/{data['pet_token']}/model_{data['gesture_id']}"
+        new_run_id = old_run_id + "_new" #새 오닉스 파일이 저장될 폴더
 
-        old_run_id = "" #db에서 가져온 오닉스 파일 저장하는 폴더 이름
-        new_run_id = "" #새 오닉스 파일이 저장될 폴더
-
+        #재학습
         cmd = f"mlagents-learn {model_name}  --initialize-from={old_run_id} --run-id={new_run_id} --env={env_name} --no-graphics"
-        os.system(cmd) #학습할때마다 뉴가 올드로 이름 바뀌어야 함
+        os.system(cmd)
+
+        #기존 모델 삭제
+        cmd = f"rm -rf {old_run_id}"
+        os.system(cmd)
+
+        #새 모델 이름 변경
+        cmd = f"mv {new_run_id} {old_run_id}"
+        os.system(cmd)
 
         #오닉스 이름: yaml파일과 동일
