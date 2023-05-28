7d2c1d919a83c8ab54e25d28d5b56ed193d0d530
Lee_Sang_Hyeon <54054162+Leeslave@users.noreply.github.com>
2023-05-28 18:33:26 +0900

    코드합침

--- Assets/Script/QRcodegenerate.cs
+++ Assets/Script/QRcodegenerate.cs
@@ -1,4 +1,3 @@
-using UnityEngine.SceneManagement;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,14 +7,14 @@ using UnityEngine.Networking;
 using System.Collections;
 using System.Text;
 using System.Data;
+using static QRcodegenerate;
 using System.Collections.Generic;
 using static System.Net.WebRequestMethods;
 using System;
 
-public class TitleManager : MonoBehaviour
+public class QRcodegenerate : MonoBehaviour
 {
-    public Camera cam;
-    public GameObject dog;
+    //
     public RawImage qrCodeImage;
     public string data = "https://klipwallet.com/?target=/a2a?request_key=";
     public string getData = "https://a2a-api.klipwallet.com/v2/a2a/result?request_key=";
@@ -28,16 +27,16 @@ public class TitleManager : MonoBehaviour
         public string expiration_time;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        qrCodeImage.enabled = false;
+        // Post prepare request키 받아오기
         StartCoroutine(UnityWebRequestPostTest());
     }
 
     IEnumerator UnityWebRequestPostTest()
     {
         string url = "https://a2a-api.klipwallet.com/v2/a2a/prepare";
+        string result;
         // Create the JSON data to send
         string json = "{\"bapp\": {\"name\": \"METADOGS\"}, \"callback\": {\"success\": \"mybapp://klipwallet/success\", \"fail\": \"mybapp://klipwallet/fail\"}, \"type\": \"auth\"}";
 
@@ -58,17 +57,16 @@ public class TitleManager : MonoBehaviour
         }
         else
         {
-            Debug.Log(www.downloadHandler.text);
             Status convertJson = JsonUtility.FromJson<Status>(www.downloadHandler.text);
             data += convertJson.request_key;    // Request Key가 생성되면 해당 키를 이용하여 사용자 인증 링크를 생성
             getData += convertJson.request_key;  // 사용자 인증이 완료되었는지 검증하는 링크에도 Request Key가 필요함.
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void buttonClick()
     {
-        cam.transform.LookAt(dog.transform);
+        GenerateQR();   // 클립으로 로그인 버튼을 누르면 QR코드를 생성을 함
+        StartCoroutine(GET());  // QR코드 생성과 동시에 코루틴을 실행하여 사용자 인증을 확인함
     }
 
     private void GenerateQR()   // QR코드 생성 함수
@@ -112,35 +110,8 @@ public class TitleManager : MonoBehaviour
         else
         {
             Debug.Log("Completed");  // 사용자 인증이 완료되면 완료 로그 출력
-            StartCoroutine(POSTdb());
-            
             // 이후에 NFT 강아지 선택 씬으로 이동하는 코드 작성 예정.
         }
     }
-    private IEnumerator POSTdb()
-    {
-        string url = "http://203.250.148.33:20080/db/load_pet_list";
-        string json = "{\"wallet_id\": \"1\"}";
 
-        // Create a UnityWebRequest
-        UnityWebRequest www = new UnityWebRequest(url, "POST");
-
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
-        www.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        www.downloadHandler = new DownloadHandlerBuffer();
-        www.SetRequestHeader("Content-Type", "application/json");
-
-        yield return www.SendWebRequest();
-        Debug.Log(www.downloadHandler.text);
-        SceneManager.LoadScene("PetSelect");
-    }
-
-
-    public void OnClickLogIn()
-    {
-        qrCodeImage.enabled = true;
-        GenerateQR();   // 클립으로 로그인 버튼을 누르면 QR코드를 생성을 함
-        StartCoroutine(GET());  // QR코드 생성과 동시에 코루틴을 실행하여 사용자 인증을 확인함
-        
-    }
 }
--- Assets/Scripts/SanghyeonCode/Login.cs
+++ Assets/Scripts/SanghyeonCode/Login.cs
@@ -1,4 +1,3 @@
-using UnityEngine.SceneManagement;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,10 +11,8 @@ using System.Collections.Generic;
 using static System.Net.WebRequestMethods;
 using System;
 
-public class TitleManager : MonoBehaviour
+public class Login : MonoBehaviour
 {
-    public Camera cam;
-    public GameObject dog;
     public RawImage qrCodeImage;
     public string data = "https://klipwallet.com/?target=/a2a?request_key=";
     public string getData = "https://a2a-api.klipwallet.com/v2/a2a/result?request_key=";
@@ -28,16 +25,16 @@ public class TitleManager : MonoBehaviour
         public string expiration_time;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void StartPrepareStart()
     {
-        qrCodeImage.enabled = false;
-        StartCoroutine(UnityWebRequestPostTest());
+        // Post prepare request키 받아오기
+        StartCoroutine(GetRequestKey());
     }
 
-    IEnumerator UnityWebRequestPostTest()
+    private IEnumerator GetRequestKey()
     {
         string url = "https://a2a-api.klipwallet.com/v2/a2a/prepare";
+        string result;
         // Create the JSON data to send
         string json = "{\"bapp\": {\"name\": \"METADOGS\"}, \"callback\": {\"success\": \"mybapp://klipwallet/success\", \"fail\": \"mybapp://klipwallet/fail\"}, \"type\": \"auth\"}";
 
@@ -59,19 +56,21 @@ public class TitleManager : MonoBehaviour
         else
         {
             Debug.Log(www.downloadHandler.text);
-            Status convertJson = JsonUtility.FromJson<Status>(www.downloadHandler.text);
+            result = www.downloadHandler.text;
+            Status convertJson = JsonUtility.FromJson<Status>(result);
+            Debug.Log(convertJson.request_key);
             data += convertJson.request_key;    // Request Key가 생성되면 해당 키를 이용하여 사용자 인증 링크를 생성
             getData += convertJson.request_key;  // 사용자 인증이 완료되었는지 검증하는 링크에도 Request Key가 필요함.
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoginBtnClick()
     {
-        cam.transform.LookAt(dog.transform);
+        GenerateQR();   // 클립으로 로그인 버튼을 누르면 QR코드를 생성을 함
+        StartCoroutine(GET());  // QR코드 생성과 동시에 코루틴을 실행하여 사용자 인증을 확인함
     }
 
-    private void GenerateQR()   // QR코드 생성 함수
+    public void GenerateQR()   // QR코드 생성 함수
     {
         int width = 256;
         int height = 256;
@@ -112,35 +111,8 @@ public class TitleManager : MonoBehaviour
         else
         {
             Debug.Log("Completed");  // 사용자 인증이 완료되면 완료 로그 출력
-            StartCoroutine(POSTdb());
-            
             // 이후에 NFT 강아지 선택 씬으로 이동하는 코드 작성 예정.
         }
     }
-    private IEnumerator POSTdb()
-    {
-        string url = "http://203.250.148.33:20080/db/load_pet_list";
-        string json = "{\"wallet_id\": \"1\"}";
 
-        // Create a UnityWebRequest
-        UnityWebRequest www = new UnityWebRequest(url, "POST");
-
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
-        www.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        www.downloadHandler = new DownloadHandlerBuffer();
-        www.SetRequestHeader("Content-Type", "application/json");
-
-        yield return www.SendWebRequest();
-        Debug.Log(www.downloadHandler.text);
-        SceneManager.LoadScene("PetSelect");
-    }
-
-
-    public void OnClickLogIn()
-    {
-        qrCodeImage.enabled = true;
-        GenerateQR();   // 클립으로 로그인 버튼을 누르면 QR코드를 생성을 함
-        StartCoroutine(GET());  // QR코드 생성과 동시에 코루틴을 실행하여 사용자 인증을 확인함
-        
-    }
 }
