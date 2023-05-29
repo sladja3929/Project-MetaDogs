98d9d09866d8d37abddf9bae78973ec850aada34
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2023-05-29 20:16:38 +0900

    kas api와 db 연동

--- relay_api/src/relayMetaLand.py
+++ relay_api/src/relayMetaLand.py
@@ -5,17 +5,18 @@ from requests import Session
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
+from kasModule import nft_list_url, x_chain_id, authorization, contract_address
+from dbModule import Database
+
 RelayMetaLand = Namespace(
     name="RelayMetaLand",
     description="메타랜드 중계 api",
 )
 
-@RelayMetaLand.route('/sync_to_db')
-class MetaLand_Sync(Resource):
-    def post(self):
-        # Get JSON data from request
-        data = request.get_json()
-        
+@RelayMetaLand.route('/pet_sync_db')
+class Pet_Sync_DB(Resource):
+    def post(self):      
+        #세션 설정  
         retries = 3
         backoff_factor = 0.3
         status_forcelist = (500, 400)
@@ -33,10 +34,57 @@ class MetaLand_Sync(Resource):
         session.mount("http://", adapter)
         session.mount("https://", adapter)
 
-        url = "http://jsonplaceholder.typicode.com/users"
-
+        headers = {
+            'x-chain-id': x_chain_id,
+            'Authorization': authorization
+        }
+        
         # Send JSON data to external server
-        response = session.get(url, timeout=3)
+        response = session.get(nft_list_url, headers=headers, timeout=3)
+        data = response.json()
 
-        # Return response from external server to client
-        return jsonify(response.json())
+        if response.status_code == 200:
+            # Access the JSON response content as a dictionary
+            data = response.json()
+
+            extras = []
+            db = Database()
+            #메타랜드 컬렉션에 속한 토큰만 추출
+            for item in data['items']:
+                if item['contractAddress'] == contract_address:
+                    
+                    #tmp nickname
+                    nickname = "tmpName"
+
+                    #pet property 추출
+                    pet_name = "tmpName"
+                    pet_age = 4
+                    pet_sex = "f"
+                    pet_emotion = "1.1"
+
+                    #db user.user에 항목 추가
+                    sql = "INSERT INTO user.user(wallet_id, nickname) SELECT %s, %s \
+                        FROM DUAL WHERE NOT EXISTS \
+                        (SELECT wallet_id, nickname FROM user.user WHERE wallet_id=%s AND nickname=%s)"
+                    db.execute(sql, (item['lastTransfer']['transferFrom'], nickname, item['lastTransfer']['transferFrom'], nickname))
+                    db.commit()
+
+                    #db nft.pet에 항목 추가
+                    sql = "INSERT INTO nft.pet(wallet_id, pet_token, pet_name, pet_age, pet_sex, pet_emotion) \
+                        VALUES(%s, %s ,%s, %s, %s, %s) \
+                        ON DUPLICATE KEY UPDATE wallet_id=%s, pet_token=%s, pet_name=%s, pet_age=%s, pet_sex=%s, pet_emotion=%s"
+                    db.execute(sql, (item['lastTransfer']['transferFrom'], item['extras']['tokenId'], pet_name, pet_age, pet_sex, pet_emotion, item['lastTransfer']['transferFrom'], item['extras']['tokenId'], pet_name, pet_age, pet_sex, pet_emotion))
+                    db.commit()
+
+                    #db log.aimodel에 항목 추가
+                    sql = "INSERT INTO log.aimodel(pet_token) SELECT %s \
+                        FROM DUAL WHERE NOT EXISTS \
+                        (SELECT pet_token FROM log.aimodel WHERE pet_token=%s)"
+                    db.execute(sql, (item['extras']['tokenId'], item['extras']['tokenId']))
+                    db.commit()
+
+            return "success"
+
+        else:
+            print('Request failed with status code:', response.status_code)
+            return response.status_code
